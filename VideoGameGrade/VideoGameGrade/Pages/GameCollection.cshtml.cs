f353405a9f92525d0193a006d135378de5ea8f33
Josh Werlein <jwerlein2@student.cvtc.edu>
2024-04-17 11:08:51 -0500

    Connect search button to Game:Collection page

--- VideoGameGrade/VideoGameGrade/Pages/GameCollection.cshtml.cs
+++ VideoGameGrade/VideoGameGrade/Pages/GameCollection.cshtml.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace VideoGameGrade.Pages
 {
     public class GameCollectionModel : PageModel
     {
         private readonly ILogger<GameCollectionModel> _logger;
+
+        public bool DatabaseConnected { get; set; }
 
         public GameCollectionModel(ILogger<GameCollectionModel> logger)
         {
@@ -14,6 +17,24 @@ namespace VideoGameGrade.Pages
 
         public void OnGet()
         {
+             
+             // Construct the connection string
+             string connectionString = "Server=videogamegrade.mysql.database.azure.com;Uid=gamegradeadmin;Pwd=capstone2024!;SslMode=Required;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    DatabaseConnected = connection.State == System.Data.ConnectionState.Open;
+                }
+                catch (Exception ex)
+                { 
+                    _logger.LogError(ex, "An error occurred in GameCollection.");
+              
+                    DatabaseConnected = false;
+                }
+            }
         }
     }
 }
